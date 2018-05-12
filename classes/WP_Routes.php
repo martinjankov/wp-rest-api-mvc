5948fbd75ff39886ec3bd31c89982a9476cc8dfa
Martin <martinjankov7@gmail.com>
2018-05-12 19:40:57 +0200

    Added resource method and args param to request

--- classes/WP_Routes.php
+++ classes/WP_Routes.php
@@ -30,26 +30,38 @@ class WP_Routes {
 		@include WPRMVC_ROUTES_DIR . $file . '.php';
 	}
 
-	public function get( $route, $function ) {
+	public function get( $route, $function, $args = array() ) {
 		try {
-			$this->request('GET', $route, $function );
+			$this->request( WP_REST_Server::READABLE, $route, $function, $args );
+			return $this;
 		} catch ( Exception $e ) {}
 	}
 
-	public function post( $route, $function ) {
+	public function post( $route, $function, $args = array() ) {
 		try {
-			$this->request('POST', $route, $function );
+			$this->request( WP_REST_Server::CREATABLE, $route, $function, $args );
+			return $this;
 		} catch ( Exception $e ) {}
 	}
 
-	public function request( $requestMethod, $route, $function ) {
-		if ( strpos( $function, '@' ) === false ) {
+	public function resource( $slug, $function, $args ) {
+		$this->request( 'resource', $slug, $function, $args );
+	}
+
+	public function request( $requestMethod, $route, $function, $args = array() ) {
+		$allowed_request_metods = array('GET', 'POST', 'PUT', 'FETCH', 'DELETE');
+
+		if ( strpos( $function, '@' ) === false && in_array( $requestMethod, $allowed_request_metods )  ) {
 			throw new Exception('Invalid or method does not exist', 404 );
 		}
 
-		$function_parts = explode('@', $function );
-		$controller = $function_parts[0];
-		$method = $function_parts[1];
+		$controller = $function;
+
+		if ( $resource !== 'resource' ) {
+			$function_parts = explode('@', $function );
+			$controller = $function_parts[0];
+			$method = $function_parts[1];
+		}
 
 		if ( ! class_exists( $controller ) ) {
 			throw new Exception('Class ' . $controller . ' does not exist', 404 );
@@ -57,9 +69,17 @@ class WP_Routes {
 
 		$route = '/' . ltrim( $route, '/' );
 
-		register_rest_route( $this->namespace . '/' . $this->version, $route, array(
+		// create resource routes
+
+		$body = array(
 		    'methods' => $requestMethod,
 		    'callback' => array( $controller, $method ),
-		) );
+		)
+
+		if ( ! empty( $args ) ) {
+			$body = array_merge( $body, $args );
+		}
+
+		register_rest_route( $this->namespace . '/' . $this->version, $route, $body );
 	}
 }
